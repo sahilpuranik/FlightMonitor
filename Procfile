6c2cfb57bf2c33a74f414b040eaa0a4dc77f3a80
Sahil Puranik <sahilpuranik@Sahils-MacBook-Pro-2.local>
2025-09-22 22:25:57 -0700

    fix python dependencies for railway

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: ./start.sh
+web: cd backend && pip install -r requirements.txt && python -m uvicorn main:app --host 0.0.0.0 --port $PORT
--- start.sh
+++ start.sh
@@ -7,7 +7,8 @@ npm run build
 
 # Install backend dependencies
 cd ../backend
+pip install --upgrade pip
 pip install -r requirements.txt
 
 # Start the backend server
-uvicorn main:app --host 0.0.0.0 --port $PORT
+python -m uvicorn main:app --host 0.0.0.0 --port $PORT
